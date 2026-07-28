c9dc0f64d3e83819a3bdf2ce2a1ef33ea8306ec3
Kalpana Pawar <madhupawar0926@gmail.com>
2026-07-28 17:38:58 +0530

    final

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer =()=>{
     return(
-        <h3 className="text-center font-semibold text-sm border border-gray-300 py-2">Crypto Tracker 2025</h3>
+        <h3 className="text-center font-semibold text-sm border border-gray-300 py-2">Crypto Tracker 2026</h3>
     )
 }
 
