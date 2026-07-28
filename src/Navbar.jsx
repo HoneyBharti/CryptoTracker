5f5c6a25bb947bcd9f82b07ac76ae17f143e8b8c
Kalpana Pawar <madhupawar0926@gmail.com>
2026-07-28 17:47:19 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar=()=>{
 
     return(
               <nav className="w-full shadow-xl p-4 bg-transparent">
-<Link to={"/"} > <h1 className="font-bold text-3xl text-center text-white">CRYPTOTRACKER</h1>
+<Link to={"/"} > <h1 className="font-bold text-3xl text-center text-white">CryptoTracker</h1>
 </Link>
 
       </nav>
